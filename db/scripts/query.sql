c1e8c6c92d49b5d789c7d896f968470a7f368f45
fadd3079-prog <fadd3079@gmail.com>
2026-06-22 16:54:19 +0700

    apdet

---
+++ db/scripts/query.sql
@@ -0,0 +1,6 @@
+-- Jalankan file ini sebagai user/schema pemilik tabel, misalnya TRANSPORTASI.
+-- File ini membuat tabel terlebih dahulu, lalu mengisi seed data.
+
+@@01_schema.sql
+@@02_seed.sql
+select * from PELANGGAN;
